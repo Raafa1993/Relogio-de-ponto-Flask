a8dd80e8184ebf7b9eca100c102f3727d97441f4
Raafa1993 <rafaelajds@gmail.com>
2020-05-18 13:13:55 -0300

    commit para producao depois de verificar que tudo deu certo

--- flask_app.py
+++ flask_app.py
@@ -9,3 +9,9 @@ def inicio():
 @app.route('/inicio-alternativo')
 def outro_inicio():
     return render_template('alternative.html')
+
+
+## Para rodar o projeto em desenvolvimento localmente
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
