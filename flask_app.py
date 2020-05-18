8cc6d7262b4c50e9f4c408cdd7077703ed5db7d5
Raafa1993 <rafaelajds@gmail.com>
2020-05-18 20:46:30 -0300

    Projeto finalizado

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,6 +19,18 @@ class Apontamento(db.Model):
     nome_usuario = db.Column(db.String(25), nullable=True)
     hora = db.Column(db.DateTime, default=datetime.now)
 
+#class Usuario (db.Model):
+#    login =
+#    email =
+
+    @property
+    def serializar(self): # para converter obj python em json
+        return {
+            'id': self.id, 
+            'nome_usuario': self.nome_usuario,
+            'hora': str(self.hora)[11:19]
+        }
+
 db.create_all()
 
 @app.route('/inicio')
@@ -26,7 +38,21 @@ db.create_all()
 def mostra_inicio():
     return render_template('index.html')
 
-@app.route('/api/funcionarios', methods=['GET', 'POST'])
+@app.route('/funcionarios', methods=['GET', 'POST'])
+def funcionarios():
+    if request.method == 'POST':
+        print(request.form['nome']) # pessoa digitou
+
+        usuario = Apontamento(nome_usuario=request.form['nome'])
+        db.session.add(usuario)
+        db.session.commit()
+        return render_template('index.html')
+
+    if request.method == 'GET':
+        ultimos10 = Apontamento.query.order_by(-Apontamento.id).limit(10).all()
+        print(ultimos10)
+        return jsonify(funcionarios=[i.serializar for i in ultimos10]) # obj python convertido em json
+
 
 @app.route('/contato')
 def contato():
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
 // Fazendo funcionar um relogio
 
 function atualizaHora() {
-  var r = document.getElementById("relogio");
-  var hoje = new Date();
-  var h = hoje.getHours();
-  var m = hoje.getMinutes();
-  var s = hoje.getSeconds();
-  if (h < 10) h = "0" + h;
-  if (m < 10) m = "0" + m;
-  if (s < 10) s = "0" + s;
-  r.textContent = h + ":" + m + ":" + s;
+    var r = document.getElementById("relogio");
+    var hoje = new Date();
+    var h = hoje.getHours();
+    var m = hoje.getMinutes();
+    var s = hoje.getSeconds();
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
+    r.textContent = h + ":" + m + ":" + s;
 }
 
 setInterval(atualizaHora, 1000); // ativa a função a cada milisegundo
@@ -39,29 +39,29 @@ const raiz = document.getElementById("lista");
 
 // xhr.send();
 
-fetch("http://juliovasque.pythonanywhere.com/api/funcionarios") // GET por default
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    data.funcionarios.forEach((element) => {
-      // criamos a linha da tabela
-      var linha = document.createElement("tr");
-      raiz.appendChild(linha);
+fetch("/funcionarios") // GET por default
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        data.funcionarios.forEach((element) => {
+            // criamos a linha da tabela
+            var linha = document.createElement("tr");
+            raiz.appendChild(linha);
 
-      // criamos as colunas
-      var funcionario = document.createElement("td");
-      funcionario.textContent = element.nome_usuario;
-      linha.appendChild(funcionario);
+            // criamos as colunas
+            var funcionario = document.createElement("td");
+            funcionario.textContent = element.nome_usuario;
+            linha.appendChild(funcionario);
 
-      var hora = document.createElement("td");
-      hora.textContent = element.data;
-      linha.appendChild(hora);
+            var hora = document.createElement("td");
+            hora.textContent = element.hora;
+            linha.appendChild(hora);
+        });
+    })
+    .catch((err) => {
+        console.log("Ocorreu um problema.");
+    })
+    .finally(() => {
+        console.log("Linha que sempre aparece no final");
     });
-  })
-  .catch((err) => {
-    console.log("Ocorreu um problema.");
-  })
-  .finally(() => {
-    console.log("Linha que sempre aparece no final");
-  });
